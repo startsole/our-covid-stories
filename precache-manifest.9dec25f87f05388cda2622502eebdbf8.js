465cd068128e4d50542fbb2921a1606786438144
Justin Duffy <jsduffy@gmail.com>
2021-01-05 10:53:50 -0500

    Updates

---
+++ precache-manifest.9dec25f87f05388cda2622502eebdbf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7549a778a9d69c8b390837c82a47b2fe",
+    "url": "/our-covid-stories/index.html"
+  },
+  {
+    "revision": "c9243c9b9c5bb844c453",
+    "url": "/our-covid-stories/static/css/2.52348831.chunk.css"
+  },
+  {
+    "revision": "9480fa7e84dd6e375779",
+    "url": "/our-covid-stories/static/css/main.778b3489.chunk.css"
+  },
+  {
+    "revision": "c9243c9b9c5bb844c453",
+    "url": "/our-covid-stories/static/js/2.0e2c9db6.chunk.js"
+  },
+  {
+    "revision": "198893cc54445754827b20e7917727d7",
+    "url": "/our-covid-stories/static/js/2.0e2c9db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9480fa7e84dd6e375779",
+    "url": "/our-covid-stories/static/js/main.b8581385.chunk.js"
+  },
+  {
+    "revision": "83d37b5dd97eb84de51f",
+    "url": "/our-covid-stories/static/js/runtime-main.92feb076.js"
+  }
+]);
